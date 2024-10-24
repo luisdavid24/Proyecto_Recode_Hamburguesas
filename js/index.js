25c13d647a1372d44fd4fbe20cd58fa09927fc60
Matiaspalaciovilla <151969724+Matiaspalaciovilla@users.noreply.github.com>
2024-10-24 17:29:40 -0500

    (update) all done

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ const img8 = document.querySelector(".img8");
 
 menuHamburguesas.map(element => {
     const box = document.createElement('div');
+    const noBtn = document.createElement('div');
     box.classList.add('product');
 
     if (element.nombre === "Hamburguesa Clásica"){
@@ -43,25 +44,38 @@ menuHamburguesas.map(element => {
     name.textContent = element.nombre;
 
     const description = document.createElement('p');
-    description.textContent = element.descripcion;
+    description.textContent = `𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝗰𝗶𝗼́𝗻: ${element.descripcion}`;
 
     const price = document.createElement('p');
-    price.textContent = element.precio;
+    price.textContent = `𝗣𝗿𝗲𝗰𝗶𝗼: ${element.precio}`;
 
-    const extras = document.createElement('ul');
-    element.adicionales.map(adicional => {
-        const li = document.createElement('li');
-        li.style.listStyle = ('none')
-        li.textContent = adicional;
-        extras.appendChild(li);
-    });
+//    const extras = document.createElement('ul');
+//    element.adicionales.map(adicional => {
+//        const li = document.createElement('li');
+//        li.style.listStyle = ('none')
+//        li.textContent = adicional;
+//        extras.appendChild(li);
+//    });
 
-    box.appendChild(name);
-    box.appendChild(description);
-    box.appendChild(price);
-    box.appendChild(extras);
+    const buyBtn = document.createElement("button");
+    buyBtn.textContent = 'AÑADIR AL CARRITO';
+    buyBtn.classList.add('buy');
+    
+    noBtn.appendChild(name);
+    noBtn.appendChild(description);
+    noBtn.appendChild(price);
+//    noBtn.appendChild(extras);
+    box.appendChild(noBtn);
 
-        
-        
+    box.appendChild(buyBtn);
     menu.appendChild(box);
+
+    buyBtn.addEventListener('click', function() {
+      localStorage.setItem("name", JSON.stringify(name.textContent));
+      localStorage.setItem("description", JSON.stringify(description.textContent));
+      localStorage.setItem("price", JSON.stringify(price.textContent));
+      
 });
+
+});
+
