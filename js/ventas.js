8ca6ca920a83995a7309e5c0aa4b33ca507c18b1
Maikandroi22 <elmaik386@gmail.com>
2024-10-24 18:42:42 -0500

    grafica progreso

---
+++ js/ventas.js
@@ -0,0 +1,42 @@
+const tiposDeHamburguesas = ['Clásica', 'Doble', 'Picante', 'Deluke', 'Vegatariana', 'Pescado', 'BBQ', 'Mediterrania'];
+const ventasHamburguesas = [15, 30, 12, 22, 8, 18, 25, 10]; 
+
+const ctx = document.getElementById('ventasChart').getContext('2d');
+const ventasChart = new Chart(ctx, {
+    type: 'bar', 
+    data: {
+        labels: tiposDeHamburguesas, 
+        datasets: [{
+            label: 'Cantidad Vendida',
+            data: ventasHamburguesas, 
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(99, 255, 132, 0.2)',
+                'rgba(162, 54, 235, 0.2)',
+            ], 
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(99, 255, 132, 1)',
+                'rgba(162, 54, 235, 1)',
+            ], 
+            borderWidth: 1 
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true 
+            }
+        }
+    }
+});
