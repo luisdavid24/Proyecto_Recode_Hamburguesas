dd2fac1ed0033307d977782f0464cdfa2ca29173
AndrackSteven <asmuma@hotmail.com>
2024-10-24 18:39:31 -0500

    funcion de las cards en progreso

--- js/carrito.js
+++ js/carrito.js
@@ -1,23 +1,42 @@
 import { menuHamburguesas } from "../config/global.js"
 
+let precioTotal = 0;
 
 
+function modificarTexto(texto){
+  let nuevoTexto = texto.replace(/ +/g, '_');
+  let textoSinTildes = nuevoTexto.normalize("NFD").replace(/[\u0300-\u036f]/g, '');
+  return textoSinTildes;
 
+}
+
+modificarTexto("como estas canción")
 function nombre(menuHamburguesas){
     var allburgers=""
     menuHamburguesas.map(burger=>{
-          let card = ` <div class="cards">
-      <img src="img/Hamburgesa_BBQ.png" alt="">
+      precioTotal+=burger.precio
+      let imageBurger = `img/`
+      imageBurger += modificarTexto(burger.nombre)
+        imageBurger+=".png"
+        console.log(imageBurger)
+      let card = ` <div class="cards">
+      <img src=${imageBurger} alt="">
       <div class="card-content">
         <h1>${burger.nombre}</h1>
         <h4>${burger.precio}</h4>
         <p>${burger.descripcion}</p >
+      </div>
       </div>`
       allburgers+= card
     })
   
-      document.getElementById("contenedor").outerHTML=allburgers
+      document.getElementById("contenedor").innerHTML=allburgers
 }
 nombre(menuHamburguesas)
 
 console.log(menuHamburguesas)
+  
+function total(sumarPrecio){
+  burger.precio.map(sumarPrecio)
+}
+
